01f3131dabe910f3d9e2f1d0b219604faad83e5f
きみと <125853241+kimiting@users.noreply.github.com>
2024-07-08 19:48:26 +0900

    TOPと検索マークを押した時の動きを追加した

--- search-bpm/static/common.js
+++ search-bpm/static/common.js
@@ -1,17 +1,43 @@
-// スライダーの値変更
-const slider = document.getElementById('slider');
-const sliderValue = document.getElementById('sliderValue');
-// スライダーの値が変更されたときに表示を更新
-slider.addEventListener('input', function() {
-    sliderValue.textContent = slider.value;
-});
+$(function(){
+    console.log("機動成功");
 
-const myButton = document.getElementById('restart-btn');
-// ボタンがクリックされたときの処理
-myButton.addEventListener('click', function() {
-    if (myButton.textContent === 'START') {
-        myButton.textContent = 'STOP';
-    } else {
-        myButton.textContent = 'START';
-    }
+    // スライダーの値変更
+    const slider = document.getElementById('slider');
+    const sliderValue = document.getElementById('sliderValue');
+    slider.addEventListener('input', function() {
+        sliderValue.textContent = slider.value;
+    });
+
+    const myButton = document.getElementById('restart-btn');
+    myButton.addEventListener('click', function() {
+        if (myButton.textContent === 'START') {
+            myButton.textContent = 'STOP';
+        } else {
+            myButton.textContent = 'START';
+        }
+    });
+
+    $('#top-btn').on('click', function() {
+        console.log("TOPボタンの機動成功");
+        if ($('#top-main').hasClass('none')) {
+            $('#top-main').slideDown(2000, function() {
+                $('#top-main').removeClass('none');
+            });
+            $('#search-main').hide(2000, function() {
+                $('#search-main').addClass('none');
+            });
+        }
+    });
+
+    $('#search-btn').on('click', function() {
+        console.log("検索ボタンの機動成功");
+        if ($('#search-main').hasClass('none')) {
+            $('#search-main').slideDown(2000, function() {
+                $('#search-main').removeClass('none');
+            });
+            $('#top-main').hide(2000, function() {
+                $('#top-main').addClass('none');
+            });
+        }
+    });
 });
