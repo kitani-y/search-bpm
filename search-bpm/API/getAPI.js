b346af4f3f50c835422e3cb37ad89750a9dff5a7
kitani-y <nativetofu@gmail.com>
2024-07-17 14:17:04 +0900

    無効なトークンを削除しました

--- search-bpm/API/getAPI.js
+++ search-bpm/API/getAPI.js
@@ -5,7 +5,7 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
 
 async function searchTrack() {
     const trackName = document.getElementById('trackName').value;
-    const accessToken = 'BQAxohK7rmxSDY1Nk9CnpDsNQED6NvsAv6p2qofXXXrVOPC5VjvuNUCKPMSj-pSxxh4hGKP7c9fO9myWEGgHuLl_ipAhbn9MYRLIeEEpNLBFEZWmxik'; 
+    const accessToken = 'enter your token'; 
     const response = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(trackName)}&type=track`, {
         headers: {
             'Authorization': `Bearer ${accessToken}`
@@ -39,7 +39,7 @@ async function displayTrack(event) {
     
     const trackId = event.target.getAttribute('data-track-id');
     const trackName = event.target.getAttribute('data-track-name');
-    const accessToken = 'BQAxohK7rmxSDY1Nk9CnpDsNQED6NvsAv6p2qofXXXrVOPC5VjvuNUCKPMSj-pSxxh4hGKP7c9fO9myWEGgHuLl_ipAhbn9MYRLIeEEpNLBFEZWmxik'; 
+    const accessToken = 'enter your token'; 
     const response = await fetch(`https://api.spotify.com/v1/audio-analysis/${trackId}`, {
         headers: {
             'Authorization': `Bearer ${accessToken}`
