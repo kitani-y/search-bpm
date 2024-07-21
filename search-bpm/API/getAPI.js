fa6e36b9503ad48b5c70059cd22c5dec5845328e
木谷　佑摩 <123094891+kitani-y@users.noreply.github.com>
2024-07-21 15:26:49 +0900

    無効なトークンを削除しました

--- search-bpm/API/getAPI.js
+++ search-bpm/API/getAPI.js
@@ -5,7 +5,7 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
 
 async function searchTrack() {
     const trackName = document.getElementById('trackName').value;
-    const accessToken = 'BQDlcwA4d43oKVW_2q6bia_wBupqjVaT436Jb5cCi6AssiQAZGFsOH4ipvJuANmKEt1D1yheccdPdq84IQmk1-JSGJ52yzjHqMeqG7JevsFR8xDLdx8'; 
+    const accessToken = 'enter your token'; 
     const response = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(trackName)}&type=track`, {
         headers: {
             'Authorization': `Bearer ${accessToken}`
@@ -41,7 +41,7 @@ async function displayTrack(event) {
     
     const trackId = event.target.getAttribute('data-track-id');
     const trackName = event.target.getAttribute('data-track-name');
-    const accessToken = 'BQDlcwA4d43oKVW_2q6bia_wBupqjVaT436Jb5cCi6AssiQAZGFsOH4ipvJuANmKEt1D1yheccdPdq84IQmk1-JSGJ52yzjHqMeqG7JevsFR8xDLdx8'; 
+    const accessToken = 'enter your token'; 
     const response = await fetch(`https://api.spotify.com/v1/audio-analysis/${trackId}`, {
         headers: {
             'Authorization': `Bearer ${accessToken}`
